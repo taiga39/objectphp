cc14b3c8d1aab328f1ed056e6ce9b233e79c3dc6
taiga39 <ishikawa.tigers@gmail.com>
2020-09-20 21:32:36 +0900

    item用テスト作成

---
+++ tests/TestItem.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+require_once('Item.php');
+require_once('Apple.php');
+
+use PHPUnit\Framework\TestCase;
+
+class TestItem extends TestCase {
+    private $item;
+    public function setUp(): void
+    {
+        $this->item= new Apple();
+    }
+
+    function testItemPrice(){
+        $answer = 100;
+        $this->assertSame($answer, $this->item->getPrice());
+    }
+}

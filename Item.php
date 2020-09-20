b38b6e069db6b9ac063ebc33693e66e38ac18c28
taiga39 <ishikawa.tigers@gmail.com>
2020-09-20 22:15:43 +0900

    タバコ計算

--- Item.php
+++ Item.php
@@ -3,12 +3,17 @@
 abstract class Item{
     protected $price;
     protected $tax = 1.1;
+
+
     public function getPrice(){
         return $this->price;
     }
+
     public function getTax(){
         return $this->tax;
     }
+
+    
     public function calcPrice($n){
         $price = $this->getPrice();
         $tax = $this->getTax();

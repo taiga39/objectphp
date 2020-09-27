e7d28ccc0fc633869b2360c908dea268d477fd68
taiga39 <ishikawa.tigers@gmail.com>
2020-09-27 23:59:47 +0900

    タバコのテスト

--- Data.php
+++ Data.php
@@ -4,7 +4,7 @@ const ITEMDATA =
     [
         1 => ["name"=>"apple","price"=>100,"tax"=>1.1],
         2 => ["name"=>"orange","price"=>40,"tax"=>1.1],
-        3 => ["name"=>"gra@e","price"=>150,"tax"=>1.1],
+        3 => ["name"=>"grape","price"=>150,"tax"=>1.1],
         4 => ["name"=>"noriben","price"=>350,"tax"=>1.1],
         5 => ["name"=>"shakeben","price"=>400,"tax"=>1.1],
         6 => ["name"=>"tabacco","price"=>420,"tax"=>1.0],
--- tests/TestItem.php
+++ tests/TestItem.php
@@ -11,13 +11,22 @@ class TestItem extends TestCase {
     /**
      * テスト前に呼び出される初期化メソッド
      */
-    public function setUp(): void
-    {
+    // public function setUp(): void
+    // {
+    //     $this->item = new Item(1);
+    // }
+
+    function test商品の個数を入れると金額を返すテストりんご(){
         $this->item = new Item(1);
-    }
-    function testCalcItemPrice(){
         $amount = 5;
         $answer = 550;
+        $this->assertSame($answer,$this->item->calcPrice($amount));
+    }
+
+    function test商品の個数を入れると金額を返すテストたばこ(){
+        $this->item = new Item(6);
+        $amount = 2;
+        $answer = 840;
         $this->assertSame($answer,$this->item->calcPrice($amount));
     }
 }
